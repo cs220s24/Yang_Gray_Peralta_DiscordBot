46a4843419a4a7e844c34cfd7289fab80d41181f
Brandon Yang <brandon.yang1210@gmail.com>
2024-04-03 22:14:42 -0400

    added to where if you type Hello or Hi with upper case it will also respond

--- bot_hello/main.py
+++ bot_hello/main.py
@@ -13,7 +13,9 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.lower() in ['hello', 'hi', 'Hello', 'Hi']:
+    if message.content.lower() in ['hello', 'hi']:
+        await message.channel.send(f'Hello {message.author.display_name}!')
+    elif message.content.upper() in ['Hello', 'Hi']:
         await message.channel.send(f'Hello {message.author.display_name}!')
 
 client.run(TOKEN)
