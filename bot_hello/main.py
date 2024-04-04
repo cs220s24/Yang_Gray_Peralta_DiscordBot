633a33301567e3dec4cffc24c51ef54ca98d381a
Brandon Yang <brandon.yang1210@gmail.com>
2024-04-03 23:00:36 -0400

    changed the whole code

--- bot_hello/main.py
+++ bot_hello/main.py
@@ -3,27 +3,22 @@ import dotenv
 import os
 import redis
 
-from discord.ext import commands
+intents = discord.Intents.all()
+client = discord.Client(command_prefix="!", intents=intents)
 
-dotenv.load_dotenv()
-
-host = os.getenv('REDIS_HOST')
-port = os.getenv('REDIS_PORT')
-
-r = redis.Redis(host=host, port=port)
-
-intents = discord.Intents.default()
-intents.message_content = True
-
-client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-    print('Logged in as {0.user}'.format(client))
+    print("We have logged in as {0.user}".format(client))
+
 
 @client.event
 async def on_message(message):
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    if message.author == client.user:
+        return
 
-client.run(os.getenv('TOKEN'))
+    if message.content.startswith("hi"):
+        await message.channel.send("Hello!")
+
+
+client.run(os.environ["BOT_TOKEN"])
