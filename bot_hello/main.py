f06373a05dfe6d96a65eca83397ed05c2a26044e
Brandon Yang <brandon.yang1210@gmail.com>
2024-04-03 22:34:52 -0400

    changed to code so it has a special character

--- bot_hello/main.py
+++ bot_hello/main.py
@@ -17,16 +17,11 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    print('Logged in as {0.user}'.format(client))
 
 @client.event
 async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.lower() in ['hello', 'hi']:
-        await message.channel.send(f'Hello {message.author.display_name}!')
-    elif message.content.upper() in ['Hello', 'Hi']:
-        await message.channel.send(f'Hello {message.author.display_name}!')
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
 
 client.run(TOKEN)
